735843db409e51bdb56fae783b0de48c0a40395c
websilverly <victoriousmba@gmail.com>
2024-01-26 14:48:11 +0100

    this one must work

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""for i in range(0, 100):
-    if i < 10:
-        print(f'0{i}, ')
-    else:
-        print(f'{i}, ')""""
-
-
+for i in range(100):
+    print("{:02d}, ".format(i))
